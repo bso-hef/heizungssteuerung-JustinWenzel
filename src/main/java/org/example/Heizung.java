8832532aa7f55de276e172cfb8b6153188da0408
jwenzel0205 <wenzeljustin55@gmail.com>
2025-03-21 08:00:27 +0100

    Commit

---
+++ src/main/java/org/example/Heizung.java
@@ -0,0 +1,20 @@
+public class Heizung {
+    private  boolean eingeschaltet;
+    private Raum raum;
+
+    public Heizung(Raum raum) {
+        this.raum = raum;
+    }
+
+    public void schalteEin() {
+        eingeschaltet = true;
+    }
+
+    public void schalteAus() {
+        eingeschaltet = false;
+    }
+
+    public boolean isEingeschalte() {
+        return eingeschaltet;
+    }
+}
